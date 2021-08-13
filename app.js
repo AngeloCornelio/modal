062dabec4588598a5a61a7f1193c1fb41d98a140
Angelo Cornelio <cornelio.angelo@gmail.com>
2021-08-13 10:23:15 -0700

    Implement modal closing functionality

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 const openBtn = document.querySelector('.open');
+const closeBtn = document.querySelector('.close');
 const modal = document.querySelector('.overlay');
 
 openBtn.addEventListener('click', () => {
   modal.classList.add('show');
+});
+
+closeBtn.addEventListener('click', () => {
+  modal.classList.remove('show');
 });
